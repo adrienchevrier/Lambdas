17fadc2315234c0eee890dc8506189013aee988e
adrien <adrien.chevrier.pro@gmail.com>
2017-03-13 11:54:32 +0100

    map

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,39 @@
+import java.util.Comparator;
+import java.util.TreeSet;
+
 /**
  * Created by adrien on 13/03/17.
  *
  */
 public class Hello {
+    private TreeSet<String> treeSet;
+    private TreeSet<String> treeSetLambda;//treeset using lambdas
+
+    /*CONSTRUCTOR*/
+    public Hello() {
+        //Print Hello World using lambdas
+        Thread thread = new Thread(() -> System.out.println("Hello World!"));
+        thread.start();
+
+        /*
+        * The following code is uselessly complicated because we need to
+        * define a new treeSet and call the function recursively,
+        * whereas it could be written in 1 line with a stream*/
+        treeSet = new TreeSet<>(new Comparator<String>(){
+            @Override
+            public int compare(String s1, String s2){
+                return s1.compareTo(s2);
+            }
+        });
+        /*By replacing the functions with lambdas within the IDE,
+        We create correct lambda functions, this is false fir Android
+        developing, which does not manages lambdas the same way*/
+        treeSetLambda = new TreeSet<>(String::compareTo);
+    }
+
+    public static void main(String[] args) {
+        Hello hello = new Hello();
+        System.out.println("compare hello and hello:"+hello.treeSet.comparator().compare("hello","hello"));//print result of "normal" comparison
+        System.out.println("compare hello and hello with lambdas:"+hello.treeSetLambda.comparator().compare("hello","hello"));////print result of "lambda" comparison
+    }
 }
---
+++ src/Teacher.java
@@ -0,0 +1,41 @@
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+/**
+ * Created by adrien on 13/03/17.
+ */
+public class Teacher {
+    private final String name;
+    private final Gender gender;
+
+    public Teacher(String name, Gender gender) {
+        this.name = name;
+        this.gender = gender;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public enum Gender{
+        MALE,
+        FEMALE
+    }
+
+    public static void main(String[] args) {
+        //We create a map sorting teachers by gender
+        Map<Gender,List<Teacher>>teacher =(Stream.of(new Teacher("Ravault",Gender.MALE),
+                new Teacher("Soukane",Gender.FEMALE),
+                new Teacher("Palasi",Gender.FEMALE),
+                new Teacher("Segado",Gender.MALE),
+                new Teacher("Diedler",Gender.MALE))
+                        .collect(Collectors.groupingBy(Teacher::getGender))
+                );
+        System.out.println(teacher);
+    }
+
+}
